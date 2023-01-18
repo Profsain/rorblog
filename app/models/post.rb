8c3ef641e4891e6e3727c22cf3d924b6afbce3f4
Profsain <profsaincrown@gmail.com>
2023-01-18 08:43:09 +0000

    created custom method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,19 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :comments
   has_many :likes
+  after_save :increment_post_counter
 
+  # update post counter
+  def increment_post_counter
+    author.increment!(:posts_counter)
+  end
+
+  def fetch_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def fetch_recent_likes
+    likes.order(created_at: :desc).limit(5)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
   has_many :likes
+
+  # A method that returns the 3 most recent posts for a given user
+  def fetch_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
   
 end
